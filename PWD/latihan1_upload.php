f21cd0fc03b048a3f8c20e84a6cfde60e09a9975
Dara Audia <86711239+daraaudia@users.noreply.github.com>
2021-10-08 21:00:36 +0700

    Update latihan1_upload.php

--- PWD/latihan1_upload.php
+++ PWD/latihan1_upload.php
@@ -1,14 +1,14 @@
-<?php
-$lokasi_file = $_FILES['fupload']['tmp_name'];
-$nama_file = $_FILES['fupload']['name'];
-$deskripsi = $_POST['deskripsi'];
-$direktori = "C:/xampp/htdocs/PWD/$nama_file";
-if (move_uploaded_file($lokasi_file, $direktori))
+<?php // tag pembuka php
+$lokasi_file = $_FILES['fupload']['tmp_name']; // membuat variabel untuk menempatkan lokasi file
+$nama_file = $_FILES['fupload']['name']; // membuat variabel nama
+$deskripsi = $_POST['deskripsi']; // membuat variabel deskripsi
+$direktori = "C:/xampp/htdocs/PWD/$nama_file"; // membuat variabel direktori untuk menyimpan file yg akan di upload
+if (move_uploaded_file($lokasi_file, $direktori)) // percabangan if jika sesuai, maka akan berhasil
 {
 echo "Nama File: <b>$nama_file</b> berhasil di upload <br>";
 echo "Deskripsi File :<br>$deskripsi";
 }
-else {
+else { // jika tidak sesuai maka file gagal di upload
  echo "File gagal diupload";
  }
-?>
+?> // tag penutup php

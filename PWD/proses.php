8539b2775027bf93a2c3264ce81f4e13976f7216
Dara Audia <86711239+daraaudia@users.noreply.github.com>
2021-10-08 20:57:23 +0700

    Update proses.php

--- PWD/proses.php
+++ PWD/proses.php
@@ -1,16 +1,16 @@
-<?php
-$nama = $_POST['nama'];
-$alamat = $_POST['alamat'];
-$email = $_POST['email'];
-$status = $_POST['status'];
-$komentar = $_POST['komentar'];
+<?php // tag pembuka php
+$nama = $_POST['nama']; // membuat variabel nama
+$alamat = $_POST['alamat']; // membuat variabel alamat
+$email = $_POST['email']; // membuat variabel email
+$status = $_POST['status']; // membuat variabel status
+$komentar = $_POST['komentar']; // membuat variabel komentar
 
-echo "<head><title>My Guest Book</head></title>";
-$fp = fopen("guestbook.txt", "a+");
+echo "<head><title>My Guest Book</head></title>"; // menampilkan judul pada halaman sebuah website
+$fp = fopen("guestbook.txt", "a+"); // mendefinisikan variabel $fp untuk membuka file guestbook.txt dengan fungsiopen
 fputs($fp, "$nama|$alamat|$email|$status|$komentar\n");
 fclose($fp);
 echo "Terima Kasih Atas Partisipasi Anda Mengisi Buku Tamu<br>";
-echo "<a href=index.html>Isi Buku Tamu</a><br>";
-echo "<a href=lihat.php>Lihat Buku Tamu</a><br>";
+echo "<a href=tampilan.php>Isi Buku Tamu</a><br>"; // membuat hyperlink menuju tampilan.php
+echo "<a href=lihat.php>Lihat Buku Tamu</a><br>"; // membuat hyperlink menuju lihat.php
 
-?>
+?> // tag penutup php

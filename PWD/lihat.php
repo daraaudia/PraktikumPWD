e663074e2ad604763c430b0463634483c69ba8f0
Dara Audia <86711239+daraaudia@users.noreply.github.com>
2021-10-08 20:54:32 +0700

    Update lihat.php

--- PWD/lihat.php
+++ PWD/lihat.php
@@ -1,9 +1,9 @@
-<?php
-echo "<head><title>My Guest Book</title></head>";
-$fp = fopen("guestbook.txt", "r");
-echo "<table border=0>";
+<?php // tag pembuka php
+echo "<head><title>My Guest Book</title></head>"; //menampilkan judul pada halaman sebuah website
+$fp = fopen("guestbook.txt", "r"); // mendefinisikan variabel $fp untuk membuka file guestbook.txt dengan fungsiopen
+echo "<table border=0>"; // membuat table pada halaman website
 while ($isi = fgets($fp, 80)) {
-    $pisah = explode("|", $isi);
+    $pisah = explode("|", $isi); // membuat variabel $pisah yang digunakan untuk memisahkan string pada file guesbook
     echo "<tr><td>Nama</td><td>: $pisah[0]</td></tr>";
     echo "<tr><td>Alamat</td><td>: $pisah[1]</td></tr>";
     echo "<tr><td>Email</td><td>: $pisah[2]</td></tr>";
@@ -12,6 +12,6 @@ while ($isi = fgets($fp, 80)) {
     <tr><td>&nbsp;<hr></td><td>&nbsp;<hr></td></tr>";
 }
 echo "</table>";
-echo "Isi Form Buku Tamu<br><a href=tampilan.php>Klik Disini</a>";
+echo "Isi Form Buku Tamu<br><a href=tampilan.php>Klik Disini</a>"; // membuat hyperlink yang menuju ke halaman tampilan.php
 
 ?>

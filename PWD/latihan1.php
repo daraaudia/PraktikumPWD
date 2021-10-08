eda2dd8eebd10cf9964977d940703a9589085de3
Dara Audia <86711239+daraaudia@users.noreply.github.com>
2021-10-08 21:02:17 +0700

    Update latihan1.php

--- PWD/latihan1.php
+++ PWD/latihan1.php
@@ -1,6 +1,6 @@
-<form enctype="multipart/form-data" method="post" action="latihan1_upload.php">
+<form enctype="multipart/form-data" method="post" action="latihan1_upload.php"> // membuat form untuk mengupload file
     File yang diupload: 
     <input type="file"name="fupload"><br>
     Deskripsi File: <br><textarea name="deskripsi" rows="6" cols="20"></textarea><br>
-    <input type=submit value=Upload>
-</form>
+    <input type=submit value=Upload> // submit file
+</form> // tag penutup form
